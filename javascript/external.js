59abdb705a48f9bc9798fff15666ceb36f1c21e0
Jfd-57 <jfd57@txstate.edu>
2026-09-22 21:36:24 -0500

    Defined a function for the welcome message.
    Refactor welcome function to prompt for user name and display a welcome message.

--- javascript/external.js
+++ javascript/external.js
@@ -1,3 +1,14 @@
-let user_name = window.prompt("Please enter your name", "Type your name here");
+function welcome()
+{
+    let a = "Please enter your name.";
+    let b = "Type your name here.";
 
-document.getElementById("welcome-header").innerHTML = "Hello, welcome " + user_name + "!";
+    // A prompt box is used to prompt users to input a value before entering a page.
+    user_name = window.prompt(a, b);
+
+    message = "<h1>Hello, welcome to my webpage, " + user_name + "!</h1>";
+
+    return message;
+}
+
+document.write(welcome());
