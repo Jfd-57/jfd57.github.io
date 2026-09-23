7a485ad337627b3a08679b387cef54b0e2ff2840
Jfd-57 <jfd57@txstate.edu>
2026-09-22 22:13:43 -0500

    Refactor webmaps and reviews structure in external.js
    Consolidated webmaps and reviews into a single array structure for better organization and simplified the webmap_table function to iterate over the new structure.

--- javascript/external.js
+++ javascript/external.js
@@ -1,3 +1,10 @@
+var webmaps =
+[
+    ["Google Maps", "https://www.google.com/maps", "Google Maps is mainly used to help people with directions, routes, traffic, and satellite imagery. It is easy to use and straightforward for everyday users. It can show routes, travel time, traffic, and different map layers. I would make it easier to hide some of the unwanted features that can make the map feel cluttered."],
+
+    ["ArcGIS Online", "https://www.arcgis.com/index.html", "ArcGIS Online is used to create, view, share, and analyze maps. It has many useful GIS tools for mapping, finding locations, analyzing data, and measuring distance. It is useful for GIS users, researchers, and organizations, but it can be confusing for beginners. I would make the commonly used tools easier to find."]
+];
+
 function welcome()
 {
     let a = "Please enter your name.";
@@ -11,44 +18,17 @@ function welcome()
     return message;
 }
 
-var webmaps =
-[
-    ["Google Maps", "https://www.google.com/maps"],
-    ["ArcGIS Online", "https://www.arcgis.com/index.html"]
-];
-
-var reviews =
-[
-    [
-        "Google Maps is mainly used to help people with directions, routes, traffic, and satellite imagery. It is easy to use and straightforward for everyday users. It can show routes, travel time, traffic, and different map layers. I would make it easier to hide some of the unwanted features that can make the map feel cluttered."
-    ],
-    [
-        "ArcGIS Online is used to create, view, share, and analyze maps. It has many useful GIS tools for mapping, finding locations, analyzing data, and measuring distance. It is useful for GIS users, researchers, and organizations, but it can be confusing for beginners. I would make the commonly used tools easier to find."
-    ]
-];
-
 function webmap_table()
 {
     document.write("<table width=100%>");
 
-    for (var row=0; row < 2; row++)
+    for (var row=0; row < webmaps.length; row++)
     {
         document.write("<tr>");
 
-        for (var column=0; column < 3; column++)
+        for (var column=0; column < webmaps[0].length; column++)
         {
-            if (column === 0)
-            {
-                document.write("<td><a href='" + webmaps[row][1] + "' target='_blank'>" + webmaps[row][0] + "</a></td>");
-            }
-            else if (column === 1)
-            {
-                document.write("<td>" + reviews[row][0] + "</td>");
-            }
-            else
-            {
-                document.write("<td><a href='" + webmaps[row][1] + "' target='_blank'>Visit Webmap</a></td>");
-            }
+            document.write("<td>" + webmaps[row][column] + "</td>");
         }
 
         document.write("</tr>");
